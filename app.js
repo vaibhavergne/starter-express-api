54128275806730f70795dc5b2ee0a8c7feb839ea
vaibhav <shubham333v@gmail.com>
2023-07-06 09:26:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ express.use(express_pack.static(__dirname+'/apps/Task/deepthought/'));
 express.use(express_pack.static(__dirname+'/apps/Task/deepthought_1/'));
 ////////////////////////
 express.get("/",(rq,rs)=>{rs.send("Welcome"); });
-express.get("/country",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought/index.html"); });
-express.get("/htm/tasks_1/deepthought",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought_1/index.html"); });
+express.get("/Front",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought/index.html"); });
+express.get("/country",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought_1/index.html"); });
 
 express.get("/api/task/deepthought",(rq,rs)=>{rs.sendFile(__dirname+"/json/deepthought/deepthought.json"); });
 express.get("/js/tasks/deepthought/component.js",(rq,rs)=>{rs.sendFile(path.join(__dirname,"/apps/Task/deepthought/ind.js")); });
