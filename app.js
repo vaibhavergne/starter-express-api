adbcf7014245e334e2b382a38e2a7c55b7fe761d
vaibhav <shubham333v@gmail.com>
2023-09-30 07:56:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get("/js/tasks/deepthought_1/component.js",(rq,rs)=>{rs.sendFile(path.join(_
 //////////////////////////
 //////////////////////////
 //////////////////////////
-app.post("/motParam",(rq,rs)=>{let bd=rq.body;rs.end(JSON.stringify(bd)); });
+app.post("/motParam",(rq,rs)=>{let bd=rq.body;
+if(bd.hasOwnProperty("tem"))motParam.tem=bd.tem; });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
