7d23a163097025b8fefdf1ea11b95fb0575e2e5c
vaibhav <shubham333v@gmail.com>
2023-09-30 06:03:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ express.get("/",(rq,rs)=>{rs.send("Welcome"); });
 express.get("/Front",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought/index.html"); });
 express.get("/country",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought_1/index.html"); });
 express.get("/motmon",(rq,rs)=>{rs.sendFile(__dirname+"/apps/motmon/index.html"); });
-express.get("/motParam",(rq,rs)=>{rs.send(JSON.stringify(motParam)); });
+express.get("/motParam",(rq,rs)=>{rs.send(JSON.stringify(motParam));motParam.tem=Math.random(); });
 
 express.get("/api/task/deepthought",(rq,rs)=>{rs.sendFile(__dirname+"/json/deepthought/deepthought.json"); });
 express.get("/js/tasks/deepthought/component.js",(rq,rs)=>{rs.sendFile(path.join(__dirname,"/apps/Task/deepthought/ind.js")); });
 express.get("/js/tasks/deepthought_1/component.js",(rq,rs)=>{rs.sendFile(path.join(__dirname,"/apps/Task/deepthought_1/ind.js")); });
+
+
+
+//////////////////////////
+//////////////////////////
+//////////////////////////
+express.post("/motParam",(rq,rs)=>{ });
 
 express.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
