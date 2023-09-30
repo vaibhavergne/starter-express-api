98a25747cdf163a06dbe0be322b676d32e393493
vaibhav <shubham333v@gmail.com>
2023-09-30 07:38:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const path=require("path");
 ////////////////////////
 var motParam={"tem":1,"cur":2,"vol":3,"spd":4};
 ////////////////////////
+app.use(bodyParser.urlencoded({extended: true }));
 app.use(express.static(__dirname+'/apps/Task/deepthought/'));
 app.use(express.static(__dirname+'/apps/Task/deepthought_1/'));
 ////////////////////////
