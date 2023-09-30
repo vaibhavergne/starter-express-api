3f05974e90f0e835eff0a645cf7b20b64f89f1e1
vaibhav <shubham333v@gmail.com>
2023-09-30 05:57:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path=require("path");
 
 
 ////////////////////////
-let motParam={"temp":1,"curr":2,"volt":3,"spd":4};
+let motParam={"tem":1,"cur":2,"vol":3,"spd":4};
 ////////////////////////
 express.use(express_pack.static(__dirname+'/apps/Task/deepthought/'));
 express.use(express_pack.static(__dirname+'/apps/Task/deepthought_1/'));
@@ -17,6 +17,7 @@ express.get("/",(rq,rs)=>{rs.send("Welcome"); });
 express.get("/Front",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought/index.html"); });
 express.get("/country",(rq,rs)=>{rs.sendFile(__dirname+"/apps/Task/deepthought_1/index.html"); });
 express.get("/motmon",(rq,rs)=>{rs.sendFile(__dirname+"/apps/motmon/index.html"); });
+express.get("/motParam",(rq,rs)=>{rs.send(JSON.stringify(motParam)); });
 
 express.get("/api/task/deepthought",(rq,rs)=>{rs.sendFile(__dirname+"/json/deepthought/deepthought.json"); });
 express.get("/js/tasks/deepthought/component.js",(rq,rs)=>{rs.sendFile(path.join(__dirname,"/apps/Task/deepthought/ind.js")); });
