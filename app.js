be2e7effde5c647478154f3848063117bec07b97
vaibhavergne <159696246+vaibhavergne@users.noreply.github.com>
2024-02-12 09:38:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express=require("express");
 //https://github.com/shubham333v/starter-express-api/blob/main/app.js
 const app=express();
 const path=require("path");
-
+var BATCH={};
 
 ////////////////////////
 var motParam={"tem":0,"cur":0,"vol":0,"spd":0};
@@ -24,5 +24,9 @@ app.use(express.static(__dirname+"/apps/taksh/"));
 app.get("/atcop",(req,res)=>{res.sendFile(__dirname+"/apps/atcop/index.html"); });
 app.get("/atcop/param",(req,res)=>{res.end(JSON.stringify(PARAMATCOP)); });
 app.post("/atcop/param",(req,res)=>{let d=req.body;PARAMATCOP.angle=d.angle;PARAMATCOP.torque=d.torque;res.end(JSON.stringify(d)); });
+
+app.get("/atcop/batch",(req,res)=>{res.end(JSON.stringify(BATCH)); });
+app.post("/atcop/batch",(req,res)=>{let d=req.body;BATCH=d;res.end(JSON.stringify(d)); });
+
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
